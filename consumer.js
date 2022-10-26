3e05ec4df36dfb0800477ae9d24a676c154cda0f
Hadi KIRKICI <abdulhadi.kirkici@vestel.com.tr>
2022-10-26 13:09:24 +0300

    Sent serial message

--- consumer.js
+++ consumer.js
@@ -9,13 +9,13 @@ connect_rabbitmq();
 async function connect_rabbitmq(){
     try {
         const connection = await amqp.connect("amqp://localhost:5672");
-    
         const channel = await connection.createChannel();
-    
         const assertion = await channel.assertQueue("jobsQueue")
 
+        console.log("Message waitnig ...")
         channel.consume("jobsQueue", message => {
             console.log("Message", message.content.toString());
+            channel.ack(message);
         })
 
     } catch (error) {
--- publisher.js
+++ publisher.js
@@ -9,14 +9,14 @@ connect_rabbitmq();
 async function connect_rabbitmq(){
     try {
         const connection = await amqp.connect("amqp://localhost:5672");
-    
         const channel = await connection.createChannel();
-    
         const assertion = await channel.assertQueue("jobsQueue")
     
-        channel.sendToQueue("jobsQueue", Buffer.from(JSON.stringify(message)))
-
-        console.log("Sent messages: ", message);
+        setInterval(() => {
+            message.description = new Date().getTime()
+            channel.sendToQueue("jobsQueue", Buffer.from(JSON.stringify(message)))
+            console.log("Sent messages: ", message);
+        }, 1000);
     } catch (error) {
      console.log("Error", error);   
     }
