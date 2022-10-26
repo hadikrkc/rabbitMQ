db50493a2d86e4d8d52255667b901d86ce15e8c3
Hadi KIRKICI <abdulhadi.kirkici@vestel.com.tr>
2022-10-26 14:34:59 +0300

    A parametric structure was created.

--- consumer.js
+++ consumer.js
@@ -4,16 +4,18 @@ const message = {
     description: "Bu bir test mesajidir.."
 }
 
+const queueName = process.argv[2] || "jobsQueue"
+
 connect_rabbitmq();
 
 async function connect_rabbitmq(){
     try {
         const connection = await amqp.connect("amqp://localhost:5672");
         const channel = await connection.createChannel();
-        const assertion = await channel.assertQueue("jobsQueue")
+        const assertion = await channel.assertQueue(queueName)
 
         console.log("Message waitnig ...")
-        channel.consume("jobsQueue", message => {
+        channel.consume(queueName, message => {
             console.log("Message", message.content.toString());
             channel.ack(message);
         })
--- publisher.js
+++ publisher.js
@@ -4,17 +4,19 @@ const message = {
     description: "This is a test message,,"
 }
 
+const queueName = process.argv[2] || "jobsQueue"
+
 connect_rabbitmq();
 
 async function connect_rabbitmq(){
     try {
         const connection = await amqp.connect("amqp://localhost:5672");
         const channel = await connection.createChannel();
-        const assertion = await channel.assertQueue("jobsQueue")
+        const assertion = await channel.assertQueue(queueName)
     
         setInterval(() => {
             message.description = new Date().getTime()
-            channel.sendToQueue("jobsQueue", Buffer.from(JSON.stringify(message)))
+            channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)))
             console.log("Sent messages: ", message);
         }, 1000);
     } catch (error) {
